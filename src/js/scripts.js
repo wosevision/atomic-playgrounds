922693a3288127385326f5d51321b16d910ad3f0
Wose <Jacksonteather@hotmail.com>
2018-01-28 17:28:12 -0500

    ⚡️ Rocket animation work

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,20 +7,9 @@ import {
 } from 'gsap';
 import $ from 'jquery';
 import MEDIA, { MediaAwareListener } from './media-query';
+import SVG, { Animator } from './animation';
 
 window.jQuery = window.jQuery || $;
-
-/**
- * Tiny proxy class for more terse method chaining on GSAP animations
- * that need to be applied in parallel to one element.
- */
-class Animator {
-  constructor(el) { this.el = el; }
-  set(opts) { TweenMax.set(this.el, opts); return this; }
-  from(duration, opts) { TweenMax.from(this.el, dur, opts); return this; }
-  to(duration, opts) { TweenMax.to(this.el, dur, opts); return this; }
-  fromTo(duration, opts) { TweenMax.fromTo(this.el, dur, opts); return this; }
-}
 
 const HERO_MOON = document.querySelector('.hero-moon .jumbotron');
 const moonAnimator = new Animator(HERO_MOON);
@@ -33,4 +22,13 @@ mq.on(MEDIA.SM_UP, () => {
     .from(2, { ease: Bounce.easeOut, transformOrigin: '50% 50%', y: '-150%' })
     .from(6, { ease: moonSwayEase, transformOrigin: '0 -150%', x: 75 })
     .from(6, { ease: moonSwayEase, transformOrigin: '50% 0', rotationX: 15, });
+
+  TweenMax.to('#rocket_ship', 2, {
+    bezier: {
+      values: SVG.ROCKET_PATH_BEZIER,
+      type:'cubic',
+      autoRotate: true
+    },
+    ease: Linear.easeNone
+  });
 });
