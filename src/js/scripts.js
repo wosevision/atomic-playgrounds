9efce9a05267d3cea052dc454798f9e71a168bc5
Tiff <tiff.a.choong@gmail.com>
2018-01-28 16:36:00 -0500

    ⚡️ Add card SVG animation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -33,4 +33,20 @@ mq.on(MEDIA.SM_UP, () => {
     .from(2, { ease: Bounce.easeOut, transformOrigin: '50% 50%', y: '-150%' })
     .from(6, { ease: moonSwayEase, transformOrigin: '0 -150%', x: 75 })
     .from(6, { ease: moonSwayEase, transformOrigin: '50% 0', rotationX: 15, });
+
+  // $('.card-hover').on('mouseenter', function() {
+  //   let box = this.querySelector('.card-hover-box')
+  //   TweenMax.set(box, {
+  //     fill: '#ffffff',
+  //     attr: {
+  //       d: 'M400,25.27c0,0-178.21,0-200,0s-200,0-200,0V400h400V25.27z'
+  //     }
+  //   })
+  //   TweenMax.to(box, 1, {
+  //     fill: '#0696fb',
+  //     attr: {
+  //       d: 'M400,25.27c0,0-57.72-15.27-200-15.27S0,25.27,0,25.27V400h400V25.27z'
+  //     }
+  //   })
+  // });
 });
